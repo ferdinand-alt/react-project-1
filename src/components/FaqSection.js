e6f54230fa422588b46c9c2800322c45e13e831c
Ferdinand Gommers <ferdinand@MacBook-Pro-van-Ferdinand.local>
2021-04-16 17:57:43 +0200

    4.FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function FaqSection() {
+    return (
+      <div className="faq">
+        <h2>
+          Questions <span>FAQ</span>
+        </h2>
+        <div className="question">
+          <h4>What are the subsidy options?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Wich safety garanty can you give us?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>In wich festivals, events are you specialised?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Are there restrictions?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default FaqSection
